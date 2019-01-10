372c00e4793bc2ac7a8fabd954c3e2de3f8048c2
lien <lien@qq.com>
2019-01-10 21:00:01 +0800

    Initial commit

---
+++ src/main/java/com/qianfemg/git/Test.java
@@ -0,0 +1,12 @@
+package com.qianfemg.git;
+
+/**
+ * @Author lien
+ * @Date 2019-01-10
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
